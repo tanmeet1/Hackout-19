b9f44041929fcb921c1c758573a8c57d9bdf521d
Mr5 <meetranoliya094@gmail.com>
2019-10-05 03:02:54 +0530

    SignUp added

--- main.py
+++ main.py
@@ -1,16 +1,43 @@
 import kivy
 from kivy.app import App
+from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
+from kivy.uix.boxlayout import BoxLayout
 
-class IndexPage():
+# class IndexPage():
 
-class Login():
+# class Login():
 
-class SignUp():
+class SignUp(GridLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.cols = 2
+        
+        self.add_widget(Label(text="Email Id:"))
+        self.email = TextInput(multiline=False)
+        self.add_widget(self.email)
 
+        self.add_widget(Label(text="Password:"))
+        self.password = TextInput(password=True,multiline=False)
+        self.add_widget(self.password)
+
+        self.add_widget(Label())
+
+        self.signUp = Button(text="Sign Up")
+        self.signUp.bind(on_press=self.signUpButton)
+        self.add_widget(self.signUp)
+
+    def signUpButton(self,instance):
+        email = self.email.text
+        password = self.password.text
+
+        print(f"Saving details in database\nEmail: {email}\nPassword: {password}")
 
 class HealthCare(App):
     def build(self):
-        return IndexPage()
-
+        #return IndexPage()
+        return SignUp()
 if __name__ == "__main__":
     HealthCare().run()
