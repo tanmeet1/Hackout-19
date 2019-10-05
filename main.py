0cd0ea50635d8958b04aab7909d530f0d1e384aa
Mr5 <meetranoliya094@gmail.com>
2019-10-05 12:35:16 +0530

    Final Commit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import kivy
+import pandas as pd
 from kivy.app import App
 from kivy.uix.carousel import Carousel
 from kivy.uix.image import AsyncImage
@@ -12,12 +13,13 @@ from kivy.uix.screenmanager import ScreenManager, Screen
 import datetime
 import csv
 
+
 class index_main(BoxLayout):
     def __init__(self,**kwargs):
         super().__init__(**kwargs)
         Box1=BoxLayout(orientation= 'vertical', spacing = 20)
         Box1.add_widget(Label(text="HealthCare"))
-        carousel = Carousel(direction='left')
+        carousel = Carousel(direction='right')
 
         for i in range(1, 5):
             src = "res/%s.jpg" % str(i)
@@ -93,19 +95,17 @@ class Update_info(BoxLayout):
         #print(data["UID"])
         data.set_index("UID",inplace=True)
         if UserID_Data in data.index:
-            data.loc[UserID_Data]["Name"] = UserName_Data
+            data.loc[UserID_Data]['Name'] = UserName_Data
             data.loc[UserID_Data]["Time"] = time_data
             data.loc[UserID_Data]["Height"] = Height_data
             data.loc[UserID_Data]["Weight"] = Weight_data
             data.loc[UserID_Data]["Allergy"] = Allergy_data
-            #data.drop([0],axis=0)
-            print(data)
-            #data.to_csv('out.csv',index=True)
+            data.to_csv("res/Singledata User Info.csv",index=True)
         else:
-            with open('res/Singledata User Info.csv','a') as fd:
-                    writer = csv.writer(fd)
-                    writer.writerow(fields)
-
+            with open("res/Singledata User Info.csv",'a') as dataN:
+                writer = csv.writer(dataN)
+                writer.writerow(fields)
+            dataN.close()
 
         app.screenManager.current = "AfterLogin"
 
@@ -136,6 +136,8 @@ class LoginScreen(GridLayout):
     
     def callback(self, instance):
         print('\n\nLogin as : '+ self.email.text +'\nPassword : '+ self.password.text)
+        app.screenManager.current = "AfterLogin"
+
     def retback(self,instance):
         app.screenManager.current = "Index"
 
@@ -171,10 +173,12 @@ class SignUp(GridLayout):
     def retback(self,instance):
         app.screenManager.current = "Index"
 
-class Update_info(BoxLayout):
+
+class GetData(BoxLayout):
+
     def __init__(self,**kwargs):
         super().__init__(**kwargs)
-
+        #self.article_read = pd.read_csv("res/Singledata User Info.csv",names=["UID","Name","Time","Height","Weight","Allergy"])
         self.add_widget(Label(text="Patient Data"))
         self.orientation = 'vertical'
         # Grid1 = GridLayout(cols = 2)
@@ -185,43 +189,47 @@ class Update_info(BoxLayout):
         #self.add_widget(Grid1) 
         
     def update_info(self,pid):
-        self.article_read = pd.read_csv("res/Singledata User Info.csv",names =['UID','Name','Time','Height','Weight','Allergy'])
-        self.add_widget(Label(text=str(self.article_read.UID[self.article_read.UID == pid])))
-        self.add_widget(Label(text=str(self.article_read.Name[self.article_read.UID == pid])))
-        self.add_widget(Label(text=str(self.article_read.Time[self.article_read.UID == pid])))
-        self.add_widget(Label(text=str(self.article_read.Height[self.article_read.UID == pid])))
-        self.add_widget(Label(text=str(self.article_read.Weight[self.article_read.UID == pid])))
-        self.add_widget(Label(text=str(self.article_read.Allergy[self.article_read.UID == pid])))
+        #print(self.article_read.UID)
+        #print(self.article_read.UID == int(pid))
+        self.article_read = pd.read_csv("res/Singledata User Info.csv")
+        comp = self.article_read.UID == int(pid)
+        self.add_widget(Label(text="UID: "+ str(self.article_read.UID[comp].values)))
+        self.add_widget(Label(text="Name: "+str(self.article_read.Name[comp].values)))
+        self.add_widget(Label(text="Time: "+str(self.article_read.Time[comp].values)))
+        self.add_widget(Label(text="Height: "+str(self.article_read.Height[comp].values)))
+        self.add_widget(Label(text="Weight: "+str(self.article_read.Weight[comp].values)))
+        self.add_widget(Label(text="Allergy: "+str(self.article_read.Allergy[comp].values)))
         #print(self.__class__.pid)
 
 
-        
 class ScanUID(BoxLayout):
     
-        Box1=BoxLayout(orientation= 'vertical', spacing = 20)
-        Box1.add_widget(Label(text="Update Info"))
-        grid = GridLayout(cols = 2)
+    def __init__(self,**kwargs):
+        super().__init__(**kwargs)
+        self.orientation = 'vertical'
+        self.Grid1 = GridLayout(cols = 2)
+        self.Grid1.add_widget(Label(text = "Enter the Patient ID : "))
+        self.userID = TextInput(multiline = False)
+        self.Grid1.add_widget(self.userID)
 
-        grid.add_widget(Label(text="User ID :",color=(1,0,0,1)))
-        self.userID = TextInput(multiline=False)
-        grid.add_widget(self.userID)
-        grid.add_widget(Label(text="User Name :",color=(1,0,0,1)))
-        self.userName = TextInput(multiline=False)
-        grid.add_widget(self.userName)
-        grid.add_widget(Label(text="Height :",color=(1,0,0,1)))
-        self.hight = TextInput(multiline=False)
-        grid.add_widget(self.hight)
-        grid.add_widget(Label(text="Weight :",color=(1,0,0,1)))
-        self.weight = TextInput(multiline=False)
-        grid.add_widget(self.weight)
-        grid.add_widget(Label(text="Allergy :",color=(1,0,0,1)))
-        self.allergy = TextInput(multiline=False)
-        grid.add_widget(self.allergy)
+        self.add_widget(self.Grid1) 
         
-        self.Submit =  Button(text='Submit', on_press=self.Submit_Callback,color=(0,1,0,1))
-        Back =  Button(text='Back', on_press=self.Back_Callback,color=(0,1,0,1))
-        grid.add_widget(self.Submit)
-        grid.add_widget(Back)
+        self.submit = Button(text="Submit",on_press=self.submit_pid)
+        self.add_widget(self.submit)
+    
+    def submit_pid(self,instance):
+        pid = self.userID.text
+        print(pid)
+        app.dataPage.update_info(pid)
+        app.screenManager.current = "GetData"
+
+
+class AfterLogin(BoxLayout):
+    def __init__(self,**kwargs):
+        super().__init__(**kwargs)
+        view = Button(text="View Patient Data")
+        view.bind(on_press=self.getuid)
+        self.add_widget(view)
 
         add = Button(text="Add New Patient")
         add.bind(on_press=self.adduid)
@@ -231,29 +239,6 @@ class ScanUID(BoxLayout):
         app.screenManager.current = "GetUID"
     def adduid(self,instance):
         app.screenManager.current = "UpdateInfo"
-        Box1.add_widget(grid)
-        self.add_widget(Box1)
-    
-    
-    def Submit_Callback(self, instance):
-        print("Submit Clicked")
-        UserID_Data = str(self.userID.text)
-        UserName_Data = str(self.userName.text)
-        Height_data = str(self.hight.text)
-        Weight_data = str(self.weight.text)
-        Allergy_data = str(self.allergy.text)
-        time_data = str(datetime.datetime.now())
-        fields=[UserID_Data,UserName_Data,time_data,Height_data,Weight_data,Allergy_data]
-        # with open('.csv', 'a') as csvFile:
-        #     writer = csv.writer(csvFile)
-        #     writer.writerow(fields)
-        # csvFile.close()
-        app.screenManager.current = "UpdateInfo"
-
-    def Back_Callback(self, instance):
-        print("Back Clicked")
-        app.screenManager.current = "DataScreen"
-
 
 
 class HealthCare(App):
@@ -275,10 +260,20 @@ class HealthCare(App):
         signUpScreen.add_widget(self.signUpPage)
         self.screenManager.add_widget(signUpScreen)
 
-        self.UpdateInfo = Update_info()
-        UpdateInfoScreen = Screen(name="UpdateInfo")
-        UpdateInfoScreen.add_widget(self.UpdateInfo)
-        self.screenManager.add_widget(UpdateInfoScreen)
+        self.dataPage = GetData()
+        dataScreen = Screen (name = "GetData")
+        dataScreen.add_widget(self.dataPage)
+        self.screenManager.add_widget(dataScreen)
+
+        self.afterLoginPage = AfterLogin()
+        afterLoginScreen  = Screen (name = "AfterLogin")
+        afterLoginScreen.add_widget(self.afterLoginPage)
+        self.screenManager.add_widget(afterLoginScreen)
+
+        self.getUIDPage = ScanUID()
+        getUIDScreen  = Screen (name = "GetUID")
+        getUIDScreen.add_widget(self.getUIDPage)
+        self.screenManager.add_widget(getUIDScreen)
 
         self.UpdateInfo = Update_info()
         UpdateInfoScreen = Screen(name="UpdateInfo")
@@ -288,7 +283,7 @@ class HealthCare(App):
         return self.screenManager
 
 
-
+      
 if __name__ == "__main__":
     app = HealthCare()
     app.run()
